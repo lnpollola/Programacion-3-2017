4303eb3586e313c2c414c61e8da7d57193fa2c42
DamianVogel <rodolfovogel@gmail.com>
2017-04-18 21:46:35 -0300

    Cambios y cosas para hacer!

---
+++ Clase05/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$conexion = mysqli_connect("localhost","root","","ejemplouno");
+
+//var_dump($conexion);
+
+$consultatext = "Select * from usuario";
+
+$consulta = mysqli_query($conexion, $consultatext);
+
+//var_dump($consulta);
+
+$retorno = mysqli_fetch_object($consulta);
+
+var_dump($retorno);
+
+mysqli_close($conexion);
+
+//Ver repositorio de PDO de Octavio y practicar (EjemploPDO)
+
+//agregar boton y modificacion via JS en los TP
+
+//Hacer ABM en 
+
+
+?>
