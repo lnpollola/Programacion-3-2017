4624e4b78313782bbc6a298ef7488d01d2dd8586
DamianVogel <rodolfovogel@gmail.com>
2017-04-22 23:10:35 -0300

    TP de SQL

--- Clase05/TP_MySQL_DV.sql
+++ Clase05/TP_MySQL_DV.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-04-2017 a las 03:11:24
--- Versión del servidor: 10.1.19-MariaDB
--- Versión de PHP: 5.6.28
+-- Tiempo de generación: 22-04-2017 a las 23:49:23
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,98 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `utn`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `envios`
---
-
-CREATE TABLE `envios` (
-  `Numero` int(11) NOT NULL,
-  `pNumero` int(11) NOT NULL,
-  `Cantidad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE `productos` (
-  `pNumero` int(11) NOT NULL,
-  `pNombre` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `Precio` float NOT NULL,
-  `Tamaño` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `productos`
---
-
-INSERT INTO `productos` (`pNumero`, `pNombre`, `Precio`, `Tamaño`) VALUES
-(1, 'Caramelos', 1.5, 'Chico'),
-(2, 'Cigarrillos', 45.89, 'Mediano'),
-(3, 'Gaseosa', 15.8, 'Grande');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `proveedores`
---
-
-CREATE TABLE `proveedores` (
-  `Numero` int(3) NOT NULL,
-  `Nombre` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `Domicilio` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `Localidad` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `proveedores`
---
-
-INSERT INTO `proveedores` (`Numero`, `Nombre`, `Domicilio`, `Localidad`) VALUES
-(100, 'Perez', 'Peron 876', 'Quilmes'),
-(101, 'Gimenez', 'Mitre 750', 'Avellaneda'),
-(102, 'Aguirre', 'Boedo 634', 'Bernal');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `envios`
---
-ALTER TABLE `envios`
-  ADD PRIMARY KEY (`Numero`,`pNumero`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`pNumero`),
-  ADD KEY `pNumero` (`pNumero`);
-
---
--- Indices de la tabla `proveedores`
---
-ALTER TABLE `proveedores`
-  ADD PRIMARY KEY (`Numero`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `pNumero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 -- --------------------------------------------------------
 
@@ -150,4 +58,3 @@ ALTER TABLE `envios`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
