1fe2002914e78e821d10bf8ecbf075037665dab4
DamianVogel <rodolfovogel@gmail.com>
2017-04-29 23:14:29 -0300

    Falta el boton modificar y eliminar

---
+++ Clase06/abm/bajadb.php
@@ -0,0 +1,40 @@
+<?php
+
+require "clases/AccesoDatos.php";
+
+if(isset($_POST["borrar"]))
+{
+    try{
+    $obj = AccesoDatos::DameUnObjetoAcceso();
+  
+
+     $consulta =$obj->RetornarConsulta("DELETE FROM producto
+                                        WHERE codigo_barra = :codigo");
+                                        
+        
+        $consulta->bindValue(':codigo', $_POST["codBarra"], PDO::PARAM_INT);
+        
+        $consulta->execute();   
+        
+        
+    if ($consulta->rowCount() == 0)
+                {
+                    echo "El elemento no existe";
+                 }
+                    else 
+                    {
+                        echo "Se eliminaron ". $consulta->rowCount() . "filas";
+                    }
+
+                require_once "baja.php";
+
+        }
+            catch (PDOException $e)
+            {
+                echo $e->getMessage();   
+            }
+
+
+
+}
+
